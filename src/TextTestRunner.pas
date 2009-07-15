2c13ea65d7354bb8cdba6d7dff8f9e2fe4d03804
Graeme Geldenhuys <graeme@mastermaths.co.za>
2009-07-15 15:28:40 +0200

    Minor changes to get unit FPC compilable.
    Signed-off-by: Graeme Geldenhuys <graeme@mastermaths.co.za>

--- src/TextTestRunner.pas
+++ src/TextTestRunner.pas
@@ -1,60 +1,52 @@
-{ $Id: TextTestRunner.pas,v 1.28 2004/10/17 10:39:00 neuromancer Exp $ }
-{: DUnit: An XTreme testing framework for Delphi programs.
-   @author  The DUnit Group.
-   @version $Revision: 1.28 $
+{
+   DUnit: An XTreme testing framework for Delphi and Free Pascal programs.
+
+   The contents of this file are subject to the Mozilla Public
+   License Version 1.1 (the "License"); you may not use this file
+   except in compliance with the License. You may obtain a copy of
+   the License at http://www.mozilla.org/MPL/
+
+   Software distributed under the License is distributed on an "AS
+   IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+   implied. See the License for the specific language governing
+   rights and limitations under the License.
+
+   The Original Code is DUnit.
+
+   The Initial Developers of the Original Code are Kent Beck, Erich Gamma,
+   and Juancarlo Añez.
+   Portions created The Initial Developers are Copyright (C) 1999-2000.
+   Portions created by The DUnit Group are Copyright (C) 2000-2007.
+   All rights reserved.
+
+   Contributor(s):
+   Kent Beck <kentbeck@csi.com>
+   Erich Gamma <Erich_Gamma@oti.com>
+   Juanco Añez <juanco@users.sourceforge.net>
+   Chris Morris <chrismo@users.sourceforge.net>
+   Jeff Moore <JeffMoore@users.sourceforge.net>
+   Uberto Barbini <uberto@usa.net>
+   Brett Shearer <BrettShearer@users.sourceforge.net>
+   Kris Golko <neuromancer@users.sourceforge.net>
+   The DUnit group at SourceForge <http://dunit.sourceforge.net>
+   Peter McNab <mcnabp@gmail.com>
+   Graeme Geldenhuys <graemeg@gmail.com>
 }
-(*
- * The contents of this file are subject to the Mozilla Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
- *
- * The Original Code is DUnit.
- *
- * The Initial Developers of the Original Code are Kent Beck, Erich Gamma,
- * and Juancarlo Añez.
- * Portions created The Initial Developers are Copyright (C) 1999-2000.
- * Portions created by The DUnit Group are Copyright (C) 2000-2008.
- * All rights reserved.
- *
- * Contributor(s):
- * Kent Beck <kentbeck@csi.com>
- * Erich Gamma <Erich_Gamma@oti.com>
- * Juanco Añez <juanco@users.sourceforge.net>
- * Chris Morris <chrismo@users.sourceforge.net>
- * Jeff Moore <JeffMoore@users.sourceforge.net>
- * Kris Golko <neuromancer@users.sourceforge.net>
- * The DUnit group at SourceForge <http://dunit.sourceforge.net>
- * Peter McNab <mcnabp@gmail.com>
- *
- *)
 
 unit TextTestRunner;
-{$IFDEF CLR}
+
+{$IFDEF FPC}
+  {$mode delphi}{$H+}
   {$UNDEF FASTMM}
 {$ENDIF}
 
-{$UNDEF XMLLISTENER}
-{$IFNDEF VER130}
-  {$IFNDEF VER140}
-    {$IFNDEF CLR}
-      {$DEFINE XMLLISTENER}
-    {$ENDIF}
-  {$ENDIF}
-{$ENDIF}
+{.$DEFINE XMLLISTENER}
 
 interface
+
 uses
   Classes,
   TestFrameworkProxyIfaces;
-
-const
-  rcs_id :string = '#(@)$Id: TextTestRunner.pas,v 1.28 2004/10/17 10:39:00 neuromancer Exp $';
 
 type
   TRunnerExitBehavior = (
@@ -138,17 +130,21 @@ uses
   {$IFDEF XMLLISTENER}
     XMLListener,
   {$ENDIF}
-  XPVistaSupport,
+//  XPVistaSupport,
   SysUtils;
 
 const
+  {$IFDEF FPC}
+  CRLF = LineEnding;
+  {$ELSE}
   CRLF = #13#10;
+  {$ENDIF}
 
 class function TTextTestListener.IniFileName: string;
 const
   TEST_INI_FILE = 'dunit.ini';
 begin
-  result := LocalAppDataPath + TEST_INI_FILE;
+  result := {LocalAppDataPath +} TEST_INI_FILE;
 end;
 
 procedure TTextTestListener.AddSuccess(Test: ITestProxy);
@@ -459,7 +455,6 @@ function RunRegisteredTests: TTestResult;
 var
   LExitBehavior : TRunnerExitBehavior;
 begin
-
   // To run with rxbPause, use -p switch
   // To run with rxbHaltOnFailures, use -h switch
   // No switch runs as rxbContinue
@@ -471,7 +466,6 @@ begin
     LExitBehavior := rxbContinue;
 
   Result := RunTest(RegisteredTests, LExitBehavior);
-
 end;
 
 function RunRegisteredTests(const AExitBehavior: TRunnerExitBehavior): TTestResult;
